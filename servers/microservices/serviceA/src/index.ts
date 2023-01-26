96a019c61207f2bfdadac46cfbb40bcc05abc499
Sudip Mondal <sudipmondal.2002@rediffmail.com>
2023-01-26 18:00:34 +0530

    add intra service communication

--- servers/microservices/serviceA/src/index.ts
+++ servers/microservices/serviceA/src/index.ts
@@ -1,6 +1,8 @@
-import  {Request, Response, NextFunction} from "express";
+import {NextFunction, Request, Response} from "express";
+import {connectToServer, requestMonitor} from "@sudipmondal/watchman";
+import axios from "axios";
+
 const express = require("express");
-import  {connectToServer, requestMonitor} from "@sudipmondal/watchman";
 require('dotenv').config();
 
 const app = express();
@@ -11,7 +13,16 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello World! This is service A");
 });
 
-app.use((err: Error, req: Request, res: Response, next:NextFunction) => {
+app.get('/hit-b', async (req: Request, res: Response) => {
+    if (process.env.SERVICE_B_URL) {
+        const result = await axios.post(process.env.SERVICE_B_URL)
+        res.send(result.data);
+    } else {
+        res.status(503).send("Service B URL not found");
+    }
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).send('Something broke!');
     next();
 })
